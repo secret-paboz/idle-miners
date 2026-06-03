13b18a9bdb4112537cb7a403a4a0fbd5cba76caf
secret-paboz <ricocigo99@gmail.com>
2026-06-03 08:06:19 +0700

    Show offline banner if supabaseClient is not initialized

--- js/main.js
+++ js/main.js
@@ -38,6 +38,10 @@ async function boot() {
   showBootSpinner("Starting up...");
 
   initSupabase();
+  if (!window.supabaseClient) {
+  const { showCloudOfflineBanner } = await import("./ui/ui-core.js");
+  showCloudOfflineBanner();
+}
   initState();
 
   const session = await restoreSession();
